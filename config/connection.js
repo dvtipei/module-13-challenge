0646b799dcaed76266be5ee2fd1964eb5d446cfe
dtipei <tipei_denis@yahoo.com>
2022-08-22 10:58:29 -0700

    adding models and updating connection

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
-require('dotenv').config();
-
 const Sequelize = require('sequelize');
+
+require('dotenv').config();
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, 'password', {
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
